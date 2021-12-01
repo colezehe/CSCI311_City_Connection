30aa740756efa0e605933f05c9b39e5855c9df36
Cole Zehe <43831345+colezehe@users.noreply.github.com>
2021-12-01 12:50:14 -0500

    Put ReadGraph features within Graph init

--- Graph.py
+++ Graph.py
@@ -7,10 +7,36 @@ Resources: geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-alg
 
 class Graph:
     # Initializes a new graph
-    def __init__(self, numV):
+    # input f = input file name
+    def __init__(self, f):
         self.graph = []
-        self.numVertices = int(numV)
+        
+        # Turn txt file into readable string
+        data = open(f, 'r').read()
+
+        # Split into list of edge data lines
+        data = data.split("\n")
+        
+        # Keep track of largest vertex ID
+        numVertices = 0
+
+        # Loop thru each edge
+        for line in data:
+            # Get individual edge's data
+            edge = line.split(" ")
+
+            # Add edge from line data into graph
+            if len(edge) >= 4:
+                self.graph.addEdge(edge[1], edge[2], edge[3])
+                
+                # Update the highest vertex ID / total num vertices
+                if(int(edge[1]) > numVertices):
+                    numVertices = int(edge[1])
+                if(int(edge[2]) > numVertices):
+                    numVertices = int(edge[2])
+        # End of init
  
+    
     # Adds an edge to the graph
     def addEdge(self, u, v, w):
         self.graph.append([int(u), int(v), float(w)])
