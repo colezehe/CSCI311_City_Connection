f5319aa538a4a921d085894b38596726701f182f
Cole Zehe <43831345+colezehe@users.noreply.github.com>
2021-12-01 12:57:36 -0500

    Modified ExecuteThis for new graph reading format

--- ExecuteThis.py
+++ ExecuteThis.py
@@ -4,15 +4,16 @@ Main program For testing our algorithms
 """
 
 from OutputGraph import *
-from ReadGraph import *
+from Graph import *
 from Kruskal import *
 
 # CHANGE THIS EVENTUALLY
 # File name of in/output txt files
 inputFile = "SanJoaquin.txt"
 outputFile = "output.txt"
+
 # Read in graph from data file
-g = ReadGraph(inputFile)
+g = Graph(inputFile)
 
 # Get MST from Kruskal's Algorithm
 g = Kruskal(g)
