dfa4ddf9f9bd89f758b1610ddb0cc196ee7bec71
tung-tran-99 <tst008@bucknell.edu>
2021-12-02 10:48:17 -0500

    Tested Kruskal for test_1 and created simple interface in ExecuteThis.py and updated README

--- ExecuteThis.py
+++ ExecuteThis.py
@@ -7,10 +7,16 @@ from OutputGraph import *
 from Graph import *
 from Kruskal import *
 
-# CHANGE THIS EVENTUALLY
+
+DEFAULT_INPUT = "SanJoaquin.txt"
+DEFAULT_OUTPUT = "output.txt"
+
+
 # File name of in/output txt files
-inputFile = "SanJoaquin.txt"
-outputFile = "output.txt"
+inputFile = input("Where are the edges located?\n")
+inputFile = inputFile if inputFile.strip() != "" else DEFAULT_INPUT
+outputFile = input("Where will the output file located?\n")
+outputFile = outputFile if outputFile.strip() != "" else DEFAULT_OUTPUT
 
 # Read in graph from data file
 g = Graph(inputFile)
@@ -20,3 +26,5 @@ g = Kruskal(g)
 
 # Output Final Graph
 OutputGraph(outputFile, g)
+print(f"Successfully wrote graph to {outputFile}")
+
